1722b9815d851f7ce7324bdd40f8f22a44d1d8c3
echeva <yaniiecheva@gmail.com>
2022-03-02 22:09:02 -0300

    Databases settings was changed

--- clicOh/settings.py
+++ clicOh/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'clicOh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
+        # 'OPTIONS': {
+        #             'sslmode': 'require',
+        # },
     }
 }
 
