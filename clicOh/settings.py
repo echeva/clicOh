04415519ff6d2c222af1c636f9c85dbac676a2b1
echeva <yaniiecheva@gmail.com>
2022-03-02 22:34:53 -0300

    Databases settings was changed

--- clicOh/settings.py
+++ clicOh/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -77,20 +78,7 @@ WSGI_APPLICATION = 'clicOh.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.getenv('DB_USER', ''),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', ''),
-        # 'OPTIONS': {
-        #             'sslmode': 'require',
-        # },
-    }
-}
+DATABASES = {'default': dj_database_url.parse(os.getenv("DATABASE_URL", 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')), conn_max_age=600)}
 
 
 # Password validation
